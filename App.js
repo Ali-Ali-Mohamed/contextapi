fc2c9a54d9ba78d003ad7426c8b1f61a6534bf86
Ali Ali Mohamed <81182273+Ali-Ali-Mohamed@users.noreply.github.com>
2024-02-13 02:48:37 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text } from 'react-native';
 
-export default function App() {
+import Navbar from "./src/components/Navbar";
+import TodoList from "./src/components/TodoList";
+import ThemeContextProvider from "./src/contexts/ThemeContext";
+import AuthContextProvider from "./src/contexts/AuthContext";
+
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <AuthContextProvider>
+        <ThemeContextProvider>
+          <Navbar />
+          <TodoList />
+        </ThemeContextProvider>
+      </AuthContextProvider>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
+
+export default App;
